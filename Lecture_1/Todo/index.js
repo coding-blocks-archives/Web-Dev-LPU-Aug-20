da036415e1c6875e393af15014d8f97b762fc26b
Jatin Katyal <jatin.katyal13@gmail.com>
2020-08-12 10:58:55 +0530

    lecture 1

---
+++ Lecture_1/Todo/index.js
@@ -0,0 +1,43 @@
+
+const actionButton = document.getElementById('todo-action');
+const todoInput = document.getElementById('todo-input');
+const todoList = document.getElementById('todo-list');
+
+function deleteLiTodo(id) {
+  const li = document.getElementById(id);
+  li.remove();
+}
+function createLiForTodo(text) {
+  const li = document.createElement('li')
+  const textEl = document.createElement('span')
+  textEl.text = text
+  const button = document.createElement('button')
+  button.text = 'Delete'
+  button.addEventListener('click', () => {
+    li.remove()
+  })
+
+  li.appendChild(textEl)
+  li.appendChild(button)
+
+  return li
+}
+
+actionButton.addEventListener('click', () => {
+  const todoText = todoInput.value;
+  const htmlContent = createLiForTodo(todoText);
+
+  todoList.append(htmlContent)
+});
+
+
+// function a() {
+
+// }
+// const a = function () {
+
+// }
+// const a = () => {
+
+// }
+// const a = () => 1
---
+++ Lecture_1/index.js
@@ -0,0 +1,38 @@
+// function func() {
+//   console.log(a);
+//   // Hoisting
+//   {
+//     var a = 10;
+//     let b = 20;
+//     const c = 30;
+
+//     console.log(a, b, c);
+//   }
+
+//   console.log(a);
+// }
+
+
+// func()
+// console.log(a)
+
+
+// function doSomething(a) {
+//   a['name'] = 'Wat !';
+// }
+
+// const obj = {
+//   name: 'Jatin Katyal'
+// }
+// doSomething(obj);
+// console.log(obj);
+
+console.log(1 == "1") // true
+
+console.log(true == "true") // false
+
+console.log([] == 0) // true
+
+console.log([1] == 1) // true
+
+console.log('b' + 'a' + + 'a' + 'a');
